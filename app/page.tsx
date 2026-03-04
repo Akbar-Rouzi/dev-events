86fcb22538dd96cbe8837ea7c5b381efaa6468fa
Akbar <seagull826@gmail.com>
2026-03-04 17:19:06 -0500

    refactor(home): move event list into landing page and enable cache components

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,36 @@
-import ExploredBtn from "@/components/ExploredBtn";
-import EventList from "@/components/EventList";
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
+
     return (
         <section>
-            <h1 className="text-center">The Hub fpr Every Dev <br/> Event You can not Miss</h1>
-            <p className="text-center mt-5">Hackathons, Meetups, and Conference, All in One Place</p>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-            <ExploredBtn/>
+            <ExploreBtn />
 
             <div className="mt-20 space-y-7">
                 <h3>Featured Events</h3>
 
-                <EventList />
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
             </div>
         </section>
     )
-};
-export default Page
+}
+
+export default Page;
